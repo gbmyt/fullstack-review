2ad32974614d5579f51ab84dd4f4cb7e821b3620
gbmyt <gbmytabc1@gmail.com>
2022-12-08 21:34:41 -0500

    Added missing static file service to server/index.js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+var getReposByUsername = require('../helpers/github').getReposByUsername;
 let app = express();
 
 // TODO - your code here!
 // Set up static file service for files in the `client/dist` directory.
-// Webpack is configured to generate files in that directory and 
-// this server must serve those files when requested. 
+// Webpack is configured to generate files in that directory and
+// this server must serve those files when requested.
+app.use('/', express.static('client/dist'));
 
+// This route should take the github username provided
+// and get the repo information from the github API, then
+// save the repo information in the database
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
+  console.log('server index.js getRepos', getReposByUsername('gbmyt'));
+  // let data = getReposByUsername('hackreactor');
+  // res.write(JSON.stringify(data));
+  console.log('/repos POST Request Log');
+  res.end('200 OK');
 });
 
 app.get('/repos', function (req, res) {
